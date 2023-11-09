58000131a1d87624c7a4c9b1f2f88da06c4bd4f2
Makima coder <vladikmyrzin10@gmail.com>
2023-11-09 23:50:16 +0300

    fix

--- src/main/java/com/example/Pack.java
+++ src/main/java/com/example/Pack.java
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-package com.example;
-
-public class Pack {
-    private String name;
-    private String url;
-    private String urlImg;
-
-    public Pack(String name, String url, String urlImg){
-        this.name = name;
-        this.url = url;
-        this.urlImg = urlImg;
-    }
-
-    public String GetName(){
-        return name;
-    }
-
-    public String GetImg(){
-        return urlImg;
-    }
-
-    public String GetUrl(){
-        return url;
-    }
-}
-=======
 package com.example;
 
 public class Pack {
@@ -52,4 +25,3 @@ public class Pack {
 
 
 }
->>>>>>> 9aedf01a896f8264c0148b00846557bec96fb1c3
